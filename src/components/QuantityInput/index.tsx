091fd85836c68196c9b17315a39f007974e79a6a
Julius Caezar <julius.caezar25@gmail.com>
2023-05-01 22:58:30 -0300

    i left the quantityInput dynamic

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -1,16 +1,27 @@
 import { Minus, Plus } from 'phosphor-react'
-import { QuantityInputContainer, IconWrapper } from './styles'
 
-export function Quantityinput() {
+import * as C from './styles'
+
+interface QuantityInputProps {
+  quantity: number;
+  onIncrease: () => void;
+  onDecrease: () => void;
+}
+
+export function Quantityinput({
+  onIncrease,
+  onDecrease,
+  quantity
+}: QuantityInputProps) {
   return (
-    <QuantityInputContainer>
-      <IconWrapper>
+    <C.QuantityInputContainer>
+      <C.IconWrapper disabled={quantity <= 0} onClick={onDecrease}>
         <Minus size={15} weight="bold" />
-      </IconWrapper>
-      <input type="number" readOnly value={1} />
-      <IconWrapper>
+      </C.IconWrapper>
+      <input type="number" readOnly value={quantity} />
+      <C.IconWrapper onClick={onIncrease}>
         <Plus size={15} weight="bold" />
-      </IconWrapper>
-    </QuantityInputContainer>
+      </C.IconWrapper>
+    </C.QuantityInputContainer>
   )
 }
